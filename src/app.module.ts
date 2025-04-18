938c2f177756b055421110b3000ebc54f77c215e
Dali Guerfali <daliguerfali@gmail.com>
2025-04-18 19:19:05 +0100

    app module config fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CvsModule } from './cvs/cvs.module';
 import { SkillsModule } from './skills/skills.module';
 import { UsersModule } from './users/users.module';
-import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { JwtAuthMiddleware } from './middleware/jwt-auth.middleware';
 
 import * as dotenv from 'dotenv';
@@ -15,16 +15,14 @@ dotenv.config();
     UsersModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      // eslint-disable-next-line @typescript-eslint/no-require-imports
       driver: require('mysql2'),
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT || '3306', 10),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: ['dist/**/*.entity{.ts,.js}'],
+      entities: [__dirname + '/**/*.entity.ts'],
       synchronize: true,
-      dropSchema: true,
     }),
   ],
   controllers: [],
